9f880a0eda40dd85953440d839b4cbc70635a5c6
mine <79789784+downy1212@users.noreply.github.com>
2021-04-10 22:38:08 +0900

    Feath date from firestore on MemoDetailScreen

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -4,7 +4,11 @@ import {
   Text, View, StyleSheet, TouchableOpacity, Alert, FlatList,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { shape, string, instanceOf, arrayOf } from 'prop-types';
+import {
+  shape, string, instanceOf, arrayOf,
+} from 'prop-types';
+
+import { dateToString } from '../utils';
 
 export default function MemoList(props) {
   const { memos } = props;
@@ -13,11 +17,11 @@ export default function MemoList(props) {
     return (
       <TouchableOpacity
         style={styles.memoListItem}
-        onPress={() => { navigation.navigate('MemoDetai'); }}
+        onPress={() => { navigation.navigate('MemoDetai', { id: item.id }); }}
       >
         <View>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
-          <Text style={styles.memoListItemDate}>{String(item.updatedAt)}</Text>
+          <Text style={styles.memoListItemDate}>{dateToString(item.updatedAt)}</Text>
         </View>
         <TouchableOpacity
           style={styles.memoDelete}
@@ -40,7 +44,7 @@ export default function MemoList(props) {
   );
 }
 
-MemoList.prototype = {
+MemoList.propTypes = {
   memos: arrayOf(shape({
     id: string,
     bodyText: string,
